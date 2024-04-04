151b1e01e1250d5a4a09be88fc46abb2d78d34c5
Anup2601 <anupm0873@gmail.com>
2024-04-04 11:33:37 +0530

    practice on if else statement

---
+++ app2.js
@@ -0,0 +1,13 @@
+let size="XL";
+if (size=="XL") {
+    console.log("Your price is RS 250");
+}
+else if (size=="L") {
+    console.log("Your price is RS 200");
+}
+else if(size=="M"){
+    console.log("Your price is RS 100");
+}
+else if(size=="S"){
+    console.log("your price is RS 50");
+}
