02c8b57a99021a665a19446cd0c2a92003e266bd
Anup2601 <anupm0873@gmail.com>
2024-04-04 11:20:54 +0530

    practice on if statement

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-
+let color="red";
+if(color==="red"){
+    console.log("please stop");
+}
+if (color==="yellow") {
+    console.log("please slow down");
+}
+if (color==="green") {
+    console.log("please go");
+}
