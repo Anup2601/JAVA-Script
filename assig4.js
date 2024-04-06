f2d81c843e40ee6632a050df91f3c6dd7187b9ca
Anup2601 <anupm0873@gmail.com>
2024-04-06 10:25:48 +0530

    array assigment

--- assig4.js
+++ assig4.js
@@ -1,3 +1,5 @@
+// -------------------question 1----------------
+
 let arr=[2,2,2,2,2,2,2,2,2,2,2,2,1];
 let num=2;
 for(let i=0;i<arr.length;i++){
@@ -5,4 +7,39 @@ for(let i=0;i<arr.length;i++){
         arr.splice(i,1);
     }
 }
+
 console.log(arr);
+
+
+// -------------------question 2&3----------------
+let num1 = 12345678;
+let count=0;
+let copy=num1;
+let sum=0;
+while(copy>0){
+    count++;
+    digite=copy%10;
+    sum=sum+digite;
+    copy=Math.floor(copy/10);
+}
+console.log(count);
+console.log(sum);
+
+
+// -------------------question 4----------------
+num=5;
+let fac=1;
+for(let i=1;i<=5;i++){
+    fac=fac*i;
+}
+console.log(fac);
+
+// ----------------question 5-----------------
+arr=[2,5,8,4,9,7];
+lar=0;
+for(let i=1;i<=arr.length;i++){
+    if(lar<arr[i]){
+        lar=arr[i];
+    }
+}
+console.log(lar);
