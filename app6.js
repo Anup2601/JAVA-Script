2bf93644e5aa5b88f47a5936f7f2559091317a25
Anup2601 <anupm0873@gmail.com>
2024-04-06 08:16:39 +0530

    practice on array

--- app6.js
+++ app6.js
@@ -6,3 +6,15 @@ console.log(a);
 a.unshift("june");
 a.unshift("july");
 console.log(a);
+
+
+// question 2
+let array1=["january","julay","march","august"];
+array1.splice(0,2,"june","july");
+console.log(array1);
+
+// question 3
+let array2=["c","c++","html","javascript","python","java","c#","sql"];
+array2.reverse();
+console.log(array2);
+console.log(array2.indexOf("javascript"));
