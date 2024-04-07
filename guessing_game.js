5e0390695441b524eff441e27c89e5cf5e2e517a
Anup2601 <anupm0873@gmail.com>
2024-04-07 10:07:30 +0530

    guessing game

--- guessing_game.js
+++ guessing_game.js
@@ -1,16 +1,19 @@
-let guess=prompt("guess the num between 1 to 10");
-num=Math.floor(Math.random()*10)+1;
+const guess=prompt("guess the num between 1 to 10");
+const num=Math.floor(Math.random()*10)+1;
 console.log(num);
-while (true) {
-    if(num==guess){
+while(true){
+    if(num===guess){
         console.log("you guess correctly");
         break;
     }
-    if (guess=="quit") {
+    if(guess==="quit") {
         console.log("you quit !");
         break;
     }
-    if (guess!=num) {
-        prompt("wrong guess try again");
+    else if(guess<num) {
+        prompt("your guess is to small please enter large no.");
+    }
+    else{
+        prompt("your guess is to big please enter small no.")
     }
 }
