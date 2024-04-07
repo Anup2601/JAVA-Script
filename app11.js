8e6952819cbec6a49ab07a462241308d4dbba623
Anup2601 <anupm0873@gmail.com>
2024-04-07 11:05:55 +0530

    practice function argument

---
+++ app11.js
@@ -0,0 +1,16 @@
+function average(a,b,c){
+    console.log((a+b+c)/3);
+}
+
+average(2,3,4);
+average(4,3,4);
+average(8,3,4);
+average(1,3,4);
+
+function table(num) {
+    for (let index = 1; index <= 100; index++) {
+        a=num*index;
+        console.log(a);
+    }
+}
+table(5);
