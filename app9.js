bb52e4ac8d22d2facba4b335bbc3196597c7be46
Anup2601 <anupm0873@gmail.com>
2024-04-07 10:39:44 +0530

    practice function

---
+++ app9.js
@@ -0,0 +1,6 @@
+function poem(){
+    console.log("First Kiss Afterwards you had that drunk,\n drugged look my daughter used to get,\n when she had let go of my nipple, \nher mouth gone slack and her eyes \nturned vague and filmy, as though behind them \nthe milk was rising up to fill her\nwhole head, that would loll on the small\nwhite stalk of her neck so I would have to hold her\ncloser, amazed at the sheer power\n of satiety, which was nothing like the needing\n to be fed, the wild flailing and crying until she fastened   \nherself to me and made the seal tight\nbetween us, and sucked, drawing the liquid down   \n and out of my body; no, this was the crowning\n moment, this giving of herself, knowing\nshe could show me how helpless\nshe was—that’s what I saw, that night when you \npulled your mouth from mine and\nleaned back against a chain-link fence,\nin front of a burned-out church: a man\nwho was going to be that vulnerable,\nthat easy and impossible to hurt.");
+
+}
+
+poem();
