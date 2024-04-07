ab3645f8f6bed1cfacbed0527d8239b6f8ddb9ab
Anup2601 <anupm0873@gmail.com>
2024-04-07 07:06:53 +0530

    practice on oop

---
+++ aap8.js
@@ -0,0 +1,23 @@
+// ------------------question 1------------------
+let num=Math.floor(Math.random()*6)+1;
+console.log(num);
+
+
+// ---------------question 2-----------------
+const car={
+    name:"nexonev",
+    model:"tata",
+    color:"black"
+};
+console.log(car.name);
+// --------------------question 3---------------
+const person={
+    name:"anup mishra",
+    age:19,
+    city:"samastipur"
+};
+console.log(person.name);
+person.city="new york";
+person.state="bihar";
+console.log(person);
+
